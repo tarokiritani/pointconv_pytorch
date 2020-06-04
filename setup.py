12626397db7a6b758a12708463a36e61c93cbaee
Taro Kiritani <taro.kiritani@exwzd.com>
2020-06-04 00:37:13 +0000

    Add setup.py
    Add setup.py and changed folder organization so that this repo can be
    installed with pip.

---
+++ setup.py
@@ -0,0 +1,11 @@
+import setuptools
+
+setuptools.setup(
+    name="point_conv_pytorch",
+    version="0.0.1",
+    python_requires='>3.6',    
+    packages=setuptools.find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3.6',
+    ],
+)
